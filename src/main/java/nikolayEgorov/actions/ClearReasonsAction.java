9df5a900ea74f9e0d8390984ee62c6e0038b8e9a
nikolay-egorov <deathadder98@gmail.com>
2018-12-16 11:09:37 +0300

    Initial commit

---
+++ src/main/java/nikolayEgorov/actions/ClearReasonsAction.java
@@ -0,0 +1,12 @@
+package nikolayEgorov.actions;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+public class ClearReasonsAction extends AnAction {
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        // TODO: insert action logic here
+    }
+}
