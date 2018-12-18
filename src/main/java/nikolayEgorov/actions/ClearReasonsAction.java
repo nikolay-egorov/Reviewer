2b09d32309fe8e36ed6939697b48d7857b660fbc
nikolay-egorov <deathadder98@gmail.com>
2018-12-18 19:32:16 +0300

    ClearReasonsAction base

--- src/main/java/nikolayEgorov/actions/ClearReasonsAction.java
+++ src/main/java/nikolayEgorov/actions/ClearReasonsAction.java
@@ -2,11 +2,27 @@ package nikolayEgorov.actions;
 
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import nikolayEgorov.listenInterfaces.Interaction;
+import nikolayEgorov.listenInterfaces.ReasonListener;
+import nikolayEgorov.processing.ReasonFormatter;
 
-public class ClearReasonsAction extends AnAction {
+public class ClearReasonsAction extends AnAction implements Interaction {
+
+     private ReasonListener reasonListener;
+
 
     @Override
     public void actionPerformed(AnActionEvent e) {
-        // TODO: insert action logic here
+        reasonListener.resetReasons();
+    }
+
+    @Override
+    public void setReasonListener(final ReasonListener aReasonListener) {
+        reasonListener = aReasonListener;
+    }
+
+    @Override
+    public void update(final AnActionEvent aActionEvent) {
+        //TODO: implement update
     }
 }
