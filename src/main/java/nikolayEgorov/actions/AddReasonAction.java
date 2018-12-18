07bb0c6e5308406166b90a89ba925a09e96630bb
nikolay-egorov <deathadder98@gmail.com>
2018-12-18 19:25:53 +0300

    AddReasonAction base

--- src/main/java/nikolayEgorov/actions/AddReasonAction.java
+++ src/main/java/nikolayEgorov/actions/AddReasonAction.java
@@ -2,11 +2,31 @@ package nikolayEgorov.actions;
 
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import nikolayEgorov.listenInterfaces.Interaction;
+import nikolayEgorov.listenInterfaces.ReasonListener;
+import nikolayEgorov.processing.ReasonFormatter;
 
-public class AddReasonAction extends AnAction {
+public class AddReasonAction extends AnAction implements Interaction{
+
+    private final ReasonFormatter reasonFormatter;
+    private ReasonListener reasonListener;
+
+    public AddReasonAction() {
+        reasonFormatter=new ReasonFormatter();
+    }
 
     @Override
     public void actionPerformed(AnActionEvent e) {
         // TODO: insert action logic here
+    }
+
+    @Override
+    public void setReasonListener(ReasonListener aReasonListener) {
+        reasonListener=aReasonListener;
+    }
+
+    @Override
+    public void update(final AnActionEvent anEvent) {
+        anEvent.getPresentation().setEnabled(true);
     }
 }
