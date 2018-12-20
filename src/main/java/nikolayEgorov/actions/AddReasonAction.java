ac18f4f445b076c7cb7854ce3be0e4fd72c46b59
nikolay-egorov <deathadder98@gmail.com>
2018-12-20 17:06:15 +0300

    AddReason action impl

--- src/main/java/nikolayEgorov/actions/AddReasonAction.java
+++ src/main/java/nikolayEgorov/actions/AddReasonAction.java
@@ -4,6 +4,9 @@ import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import nikolayEgorov.listenInterfaces.Interaction;
 import nikolayEgorov.listenInterfaces.ReasonListener;
+import nikolayEgorov.myDialogMessage;
+import nikolayEgorov.processing.FqnMiner;
+import nikolayEgorov.processing.FqnMinerResult;
 import nikolayEgorov.processing.ReasonFormatter;
 
 /**
@@ -15,14 +18,21 @@ public class AddReasonAction extends AnAction implements Interaction{
 
     private final ReasonFormatter reasonFormatter;
     private ReasonListener reasonListener;
+    private final FqnMiner fqnMiner;
 
     public AddReasonAction() {
         reasonFormatter=new ReasonFormatter();
+        fqnMiner = new FqnMiner();
     }
 
     @Override
     public void actionPerformed(AnActionEvent e) {
-        // TODO: insert action logic here
+        final FqnMinerResult result=fqnMiner.mineFqnAtCaret(e);
+        final myDialogMessage dlg = new myDialogMessage();
+        dlg.show();
+        if (dlg.isOK()) {
+             reasonListener.AddReason(reasonFormatter.getFormattedReason(result, dlg.getComment()));
+        }
     }
 
     @Override
