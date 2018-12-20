dcfe5813871cdd25481e9a70a37c7e9736aa5c94
nikolay-egorov <deathadder98@gmail.com>
2018-12-20 23:15:54 +0300

    ReasonTransferable impl transferable

---
+++ src/main/java/nikolayEgorov/actions/ReasonTransferable.java
@@ -0,0 +1,34 @@
+package nikolayEgorov.actions;
+
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.Transferable;
+import java.awt.datatransfer.UnsupportedFlavorException;
+import java.io.IOException;
+
+/**
+ * Class needed to make Reasons being able to transfer, Hence, they would be available from clipboard in @CopyReasons
+ */
+
+public class ReasonTransferable implements Transferable {
+
+    private final String fqn;
+
+    public ReasonTransferable(String fqn) {
+        this.fqn = fqn;
+    }
+
+    @Override
+    public DataFlavor[] getTransferDataFlavors() {
+        return new DataFlavor[0];
+    }
+
+    @Override
+    public boolean isDataFlavorSupported(DataFlavor flavor) {
+        return false;
+    }
+
+    @Override
+    public Object getTransferData(DataFlavor flavor) throws UnsupportedFlavorException, IOException {
+        return null;
+    }
+}
