20541850462f353f72720986498329e3b9433d66
nikolay-egorov <deathadder98@gmail.com>
2018-12-21 10:46:46 +0300

    CopyReasons is done and integrated

--- src/main/java/nikolayEgorov/actions/CopyReasonsAction.java
+++ src/main/java/nikolayEgorov/actions/CopyReasonsAction.java
@@ -2,8 +2,10 @@ package nikolayEgorov.actions;
 
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.ide.CopyPasteManager;
 import nikolayEgorov.listenInterfaces.Interaction;
 import nikolayEgorov.listenInterfaces.ReasonListener;
+import nikolayEgorov.processing.StatusBarInfo;
 
 public class CopyReasonsAction extends AnAction implements Interaction {
 
@@ -12,6 +14,9 @@ public class CopyReasonsAction extends AnAction implements Interaction {
 
     @Override
     public void actionPerformed(AnActionEvent e) {
+        final String fqn = reasonListener.getGeneratedReasons();
+        CopyPasteManager.getInstance().setContents(new ReasonTransferable(fqn));
+        StatusBarInfo.setStatusBarInfo(e.getProject(), "All reasons copied to the clipboard.");
 
     }
 
